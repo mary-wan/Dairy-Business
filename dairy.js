93f55e3804901d39ae14c5024f3deb6146c51675
mary-wan <mary.njenga@student.moringaschool.com>
2021-08-23 11:59:40 +0300

    added js file

---
+++ dairy.js
@@ -0,0 +1,10 @@
+function milkProduc(e){
+    e.preventDefault();
+    var a = document.getElementById("sa").value;
+    var b = document.getElementById("sb").value;
+    var c = document.getElementById("sc").value;
+    var d = document.getElementById("sd").value;
+
+    
+   
+}
