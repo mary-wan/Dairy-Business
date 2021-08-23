9057b597a96b36220cb2859f306b6ac0460891d0
mary-wan <mary.njenga@student.moringaschool.com>
2021-08-23 14:53:08 +0300

    added wk and yr proj

--- dairy.js
+++ dairy.js
@@ -1,19 +1,18 @@
+var a ,b, c, d, shedTotal;
 function milkProduc(e){
     e.preventDefault();
+     a = parseFloat(document.getElementById("sa").value);
+     b = parseFloat(document.getElementById("sb").value);
+     c = parseFloat(document.getElementById("sc").value);
+     d = parseFloat(document.getElementById("sd").value);
+     shedTotal =  a+b+c+d;
 
-    let a = parseFloat(document.getElementById("sa").value);
-    let b = parseFloat(document.getElementById("sb").value);
-    let c = parseFloat(document.getElementById("sc").value);
-    let d = parseFloat(document.getElementById("sd").value);
-    let shedTotal =  a+b+c+d;
-    
-    document.getElementById("shA").innerHTML=a + " litres";
+    document.getElementById("shA").innerHTML=+a + " litres";
     document.getElementById("shB").innerHTML=b + " litres";
     document.getElementById("shC").innerHTML=c + " litres";
     document.getElementById("shD").innerHTML=d + " litres";
-    document.getElementById("shT").innerHTML=shedTotal + " litres"
+    document.getElementById("shT").innerHTML=shedTotal + " litres";   
     
-   
 }
 function shClear(){
     document.getElementById("shA").value= " ";
@@ -23,4 +22,13 @@ function shClear(){
     document.getElementById("shT").value= " ";
     
 }
+function incomeProjection(e){
+    e.preventDefault();
+    let rate = document.getElementById("bR").value;
+    let weekly = rate * 7 * shedTotal;
+    let yearly = rate * 12 * shedTotal
+    document.getElementById("wk").innerHTML= weekly + " litres";
+    document.getElementById("yr").innerHTML= weekly + " litres";
+}
+
 
