771210a3f3616b6a41edc411b5ddcf734e830546
mary-wan <mary.njenga@student.moringaschool.com>
2021-08-23 12:58:26 +0300

    added input methods for sheds

--- dairy.js
+++ dairy.js
@@ -1,10 +1,26 @@
 function milkProduc(e){
     e.preventDefault();
-    var a = document.getElementById("sa").value;
-    var b = document.getElementById("sb").value;
-    var c = document.getElementById("sc").value;
-    var d = document.getElementById("sd").value;
 
+    let a = parseFloat(document.getElementById("sa").value);
+    let b = parseFloat(document.getElementById("sb").value);
+    let c = parseFloat(document.getElementById("sc").value);
+    let d = parseFloat(document.getElementById("sd").value);
+    let shedTotal =  a+b+c+d;
+    
+    document.getElementById("shA").innerHTML=a + " litres";
+    document.getElementById("shB").innerHTML=b + " litres";
+    document.getElementById("shC").innerHTML=c + " litres";
+    document.getElementById("shD").innerHTML=d + " litres";
+    document.getElementById("shT").innerHTML=shedTotal + " litres"
     
    
 }
+function shClear(){
+    document.getElementById("shA").value= " ";
+    document.getElementById("shB").value= " ";
+    document.getElementById("shC").value= " ";
+    document.getElementById("shD").value= " ";
+    document.getElementById("shT").value= " ";
+    
+}
+
